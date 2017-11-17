6dc2bfee14f7e9c809012eb7d591140aed0b57eb
nathaly carballi corcino <nathaly9929@gmail.com>
2017-11-17 16:15:13 -0500

    add h2 tag and p tag

--- archivo.php
+++ archivo.php
@@ -4,5 +4,7 @@
  <title> SUBIR REPO A GITHUB </title>
 </head>
 <body> <h1> Solucionemos problemas</h1>
+       <p> este es mi repositorio local y va para el servidor</p> 
+       <h2> todo bien </h2> 
 </body>
 </html>
